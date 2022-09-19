de16a5e1e55f36262484ab226d01b884375e3cc7
uahmad417 <18u.ahmad@gmail.com>
2022-09-19 17:28:57 +0500

    Completed script for heartbeat client

--- PyHBClient.py
+++ PyHBClient.py
@@ -2,6 +2,9 @@ import argparse
 import logging
 from time import sleep, strftime
 from datetime import datetime
+import socket
+
+logging.basicConfig(format = '%(asctime)s - %(message)s', level = logging.INFO)
 
 parser = argparse.ArgumentParser(
     prog='PyHBClient.py',
@@ -12,7 +15,7 @@ parser.add_argument(
     '-ip',
     '--ip',
     help = 'The ip of the heartbeat server, defaults to localhost',
-    type = str
+    type = str,
 )
 
 parser.add_argument(
@@ -21,6 +24,14 @@ parser.add_argument(
     help = 'The port of the heartbeat serer, defaults to 1234'
 )
 
-args = parser.parse_args()
+args = vars(parser.parse_args())
 
-logging.basicConfig(format = '%(asctime)s - %(message)s', level = logging.INFO)
+hb_server = args['ip'] if args['ip'] is not None else '127.0.0.1'
+hb_port = args['port'] if args['port'] is not None else 1234
+
+logging.debug('Creating Socket')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+logging.info('PyHeartBeat client sending to IP %s , port %d'%(hb_server, hb_port))
+while 1:
+    sock.sendto(b'Umair',(hb_server,hb_port))
+    sleep(5)
