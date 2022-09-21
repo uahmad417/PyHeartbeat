5789369237139da9f87351b23ee597be72fc9ab7
uahmad417 <18u.ahmad@gmail.com>
2022-09-21 09:26:37 +0500

    Refactored the client script The client script instead of accepting arguments now reads values from a configuraition file. This approach is much more straightforward, easy to debug and easier to add more configuration options to.

--- PyHBClient.py
+++ PyHBClient.py
@@ -3,10 +3,11 @@ import logging
 from time import sleep, strftime
 from datetime import datetime
 import socket
+import configparser
 
-logging.basicConfig(format = '%(asctime)s - %(message)s', level = logging.INFO)
+logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(threadName)s --  %(message)s', level = logging.DEBUG, datefmt='%H:%M:%S')
 
-def cli():
+'''def cli():
     parser = argparse.ArgumentParser(
         prog='PyHBClient.py',
         description='This is the heartbeat client'
@@ -29,14 +30,20 @@ def cli():
 
     hb_server = args['ip'] if args['ip'] is not None else '127.0.0.1'
     hb_port = args['port'] if args['port'] is not None else 1234
-    return hb_server, hb_port
+    return hb_server, hb_port'''
+
+def readConfig():
+    config = configparser.ConfigParser()
+    logging.info('Reading configration file')
+    config.read('HBClient.ini')
+    return config['HBServer']['host'], eval(config['HBServer']['port'])
 
 
 if __name__ == '__main__':
-    server, port = cli()
+    server, port = readConfig()
     logging.info('Creating Socket')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    logging.info('PyHeartBeat client sending to IP %s , port %d'%(server, port))
+    logging.info(f'PyHeartBeat client sending Hello packet to IP {server} , port {port}')
     while 1:
-        sock.sendto(b'Umair',(server,port))
+        sock.sendto('Hello'.encode('utf-8'),(server,port))
         sleep(5)
