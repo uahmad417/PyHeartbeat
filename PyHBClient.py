1ef7c02f5363985c61df38629096704b8be1443b
uahmad417 <18u.ahmad@gmail.com>
2022-09-21 09:44:20 +0500

    modified code

--- PyHBClient.py
+++ PyHBClient.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(threadName)s --  %
 def readConfig():
     config = configparser.ConfigParser()
     logging.info('Reading configration file')
-    config.read('HBClient.ini')
+    config.read('config.ini')
     return config['HBServer']['host'], eval(config['HBServer']['port'])
 
 
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     server, port = readConfig()
     logging.info('Creating Socket')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    logging.info(f'PyHeartBeat client sending Hello packet to IP {server} , port {port}')
     while 1:
+        logging.info(f'PyHeartBeat client sending Hello packet to IP {server} , port {port}')
         sock.sendto('Hello'.encode('utf-8'),(server,port))
         sleep(5)
--- PyHBServer.py
+++ PyHBServer.py
@@ -5,9 +5,9 @@ from datetime import datetime, timedelta
 import socket
 import configparser
 
-logging.basicConfig(format = '%(asctime)s - %(message)s', level = logging.INFO)
+logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(threadName)s --  %(message)s', level = logging.DEBUG, datefmt='%H:%M:%S')
 
-class BeatDict:
+'''class BeatDict:
     def __init__(self):
         self.beat_dict = {}
     
@@ -25,23 +25,27 @@ class ServerSocket():
 
     def recv(self):
         data, addr = self.sock.recvfrom(10)
-        logging.info('Recieved {} from {}'.format(data,addr))
+        logging.info('Recieved {} from {}'.format(data,addr))'''
 
 def readConfig():
     config = configparser.ConfigParser()
-    logging.info('Reading configration file')
-    config.read('HBClient.ini')
+    logging.debug('Reading configration file')
+    config.read('config.ini')
     return config['HBServer']['host'], eval(config['HBServer']['port'])
 
 def main():
     server, port = readConfig()
+    logging.debug(f'Creating socket at {server} : {port}')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    logging.debug('Socket Created')
+    logging.debug(f'Binding to socket at {server} : {port}')
     sock.bind((server,port))
+    logging.debug(f'Listening on socket at {server} : {port}')
     while 1:
         data, addr = sock.recvfrom(10)
-        logging.info('Recieved {} from {}'.format(data,addr))
-        print(dir(addr))
-        sleep(2)
+        logging.info('Recieved Hello Packet from {} : {}'.format(addr[0], addr[1]))
+        #sleep(2)
 
 if __name__ == '__main__':
+    logging.debug('Program Started')
     main()
