5f116902b5f4c1521d6ff568227e3e4ebb811e59
uahmad417 <18u.ahmad@gmail.com>
2022-09-22 13:46:51 +0500

    Added dockerfile and docker compose Added a dockerfile and docker compose as a test case for the scripts.

---
+++ docker/heartbeat/PyHBClient.py
@@ -0,0 +1,31 @@
+import logging
+from time import sleep
+from datetime import datetime
+import socket
+import configparser
+
+
+
+def readConfig():
+    global config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+
+if __name__ == '__main__':
+    readConfig()
+    logging.basicConfig(
+        format='%(asctime)s -- %(levelname)s -- %(threadName)s --  %(message)s',
+        level = eval(config['Logging']['level']),
+        datefmt='%H:%M:%S')
+    logging.debug('Read configration file')
+    logging.debug('Starting Program')
+    logging.debug('Creating Socket')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    
+    while 1:
+        logging.info(f'PyHeartBeat client sending Hello packet to IP {config["HBServer"]["host"]} , port {eval(config["HBServer"]["port"])}')
+        msg = f'Hello at _ {str(datetime.now())}'
+        sock.sendto(msg.encode('utf-8'),(config['HBServer']['host'],eval(config['HBServer']['port'])))
+        sleep(eval(config["HBClient"]["HBWait"]))
